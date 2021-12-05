522c034b87d5bdd060506db9a34299319490e0a7
Bhaskar Mahor <bhaskarmahor1@gmail.com>
2021-12-05 23:19:12 +0530

    working on template

--- study_buddy/base/views.py
+++ study_buddy/base/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+rooms = [
+    {'id':1,'name':"Let's learn python"},
+    {'id':2,'name':"Design with me!"},
+    {'id':3,'name':"front end developer"},
+]
+
+def home(request):
+    context = {'rooms':rooms}
+    return render(request, 'base/home.html', context)
+
+
+def room(request,pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room':room}
+    return render(request, 'base/room.html',context)
