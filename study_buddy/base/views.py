5a12e6c86a2a5675f3dbba5de589e9c2275edca8
Bhaskar Mahor <bhaskarmahor1@gmail.com>
2021-12-14 15:30:49 +0530

    forms

--- study_buddy/base/views.py
+++ study_buddy/base/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render, redirect
 from .models import Room
 from .forms import RoomForm
 # Create your views here.
@@ -17,5 +17,12 @@ def room(request,pk):
 
 def createRoom(request):
     form = RoomForm()
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
     context = {'form':form}
     return render(request, 'base/room_form.html', context )
